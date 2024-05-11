328a20af52044099f76036cf43c85b1363ffac19
Dilan Pramodya <it21169694@my.sliit.lk>
2024-05-11 21:56:45 +0530

    create workout plan controller

---
+++ Lifted-Fitness-Application-backend/src/main/java/com/Lifted/Lifted/Fitness/Application/controller/WorkoutPlanController.java
@@ -0,0 +1,49 @@
+package com.Lifted.Lifted.Fitness.Application.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.Lifted.Lifted.Fitness.Application.entity.workoutPlan;
+import com.Lifted.Lifted.Fitness.Application.service.WorkoutPlanServices;
+
+public class WorkoutPlanController {
+
+    
+    @Autowired
+    private WorkoutPlanServices workoutPlanServices;
+
+    @PostMapping(value = "/")
+    private String saveWorkoutPlan(@RequestBody workoutPlan workoutPlan) {
+        workoutPlanServices.saveOrUpdate(workoutPlan);
+        return workoutPlan.getId();
+    }
+
+    @GetMapping(value = "/getall")
+    public Iterable<workoutPlan> getWorkoutPlans() {
+        return workoutPlanServices.listAll();
+    }
+
+    @PutMapping(value = "{id}")
+    private workoutPlan update(@RequestBody workoutPlan workoutPlan, @PathVariable(name = "id") String id) {
+        workoutPlan.setId(id);
+        workoutPlanServices.saveOrUpdate(workoutPlan);
+        return workoutPlan;
+    }
+
+    @DeleteMapping("{id}")
+    private void deleteWorkoutPlan(@PathVariable("id") String id) {
+        workoutPlanServices.deleteWorkoutPlan(id);
+    }
+
+    @RequestMapping("{id}")
+    private workoutPlan getWorkoutPlan(@PathVariable(name = "id") String id) {
+        return workoutPlanServices.getWorkoutPlanById(id);
+    }
+
+}
